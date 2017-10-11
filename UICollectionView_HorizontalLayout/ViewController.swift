e0c93c472daf070529db396e59a5751abade15ee
commany <commany@naver.com>
2017-10-11 19:10:04 +0900

    Initial Commit

---
+++ UICollectionView_HorizontalLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICollectionView_HorizontalLayout
+//
+//  Created by commany_mac on 2017. 10. 11..
+//  Copyright © 2017년 CommanyShin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
